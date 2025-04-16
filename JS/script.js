b2ddc2e2b0a9fa5e4d5f7409e0034916c5aa9c9d
Ana Clara <102690664+bax7os@users.noreply.github.com>
2025-04-16 12:21:22 -0400

    rolagem de roupas pronto js

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,24 @@
 // controle de rolagem - article rolagem de roupas
 
 document.addEventListener('DOMContentLoaded', function(){
-    const container = new document.querySelector('.cards-container');
-    const leftArrow = new document.querySelector('.left-arrow');
-    const rightArrow = new document.querySelector('.right-arrow');
+    const container =  document.querySelector('.cards-container');
+    const leftArrow =  document.querySelector('.left-arrow');
+    const rightArrow =  document.querySelector('.right-arrow');
 
     function checkScroll() {
         const scrollLeft = container.scrollLeft;
-        const maxScroll = container.scrollWidht - container.clientWidth;
+        const maxScroll = container.scrollWidth - container.clientWidth;
 
-        leftArrow.classList.toggle('hidden', scrollLeft === 0);
+        
         rightArrow.classList.toggle('hidden', scrollLeft >= maxScroll - 5);
     }
     leftArrow.addEventListener('click', () => {
         container.scrollBy({ left: -300, behavior: 'smooth' });
     });
-
+    rightArrow.addEventListener('click', () => {
+        container.scrollBy({ left: 300, behavior: 'smooth' });
+    });
+    container.addEventListener('scroll', checkScroll);
+    checkScroll();
 
 });
