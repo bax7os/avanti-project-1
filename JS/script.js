a02abdbe59b2cba8e38ab5a2cb0ec2c0cad81b5e
Ana Clara <102690664+bax7os@users.noreply.github.com>
2025-04-16 12:02:57 -0400

    arrow javascript

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,20 @@
 // controle de rolagem - article rolagem de roupas
 
-document.addEventListener('DOMContentLoaded')
+document.addEventListener('DOMContentLoaded', function(){
+    const container = new document.querySelector('.cards-container');
+    const leftArrow = new document.querySelector('.left-arrow');
+    const rightArrow = new document.querySelector('.right-arrow');
+
+    function checkScroll() {
+        const scrollLeft = container.scrollLeft;
+        const maxScroll = container.scrollWidht - container.clientWidth;
+
+        leftArrow.classList.toggle('hidden', scrollLeft === 0);
+        rightArrow.classList.toggle('hidden', scrollLeft >= maxScroll - 5);
+    }
+    leftArrow.addEventListener('click', () => {
+        container.scrollBy({ left: -300, behavior: 'smooth' });
+    });
+
+
+});
